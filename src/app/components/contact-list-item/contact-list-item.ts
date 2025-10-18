75c449ed302d849f7499fa2b69f5f1891fb16a0a
Jenaro <jenaromoretti2005@gmail.com>
2025-10-18 18:14:38 -0300

    Borrar/editar contacto desde contact-page. Funcionalidades basicas terminadas

--- src/app/components/contact-list-item/contact-list-item.ts
+++ src/app/components/contact-list-item/contact-list-item.ts
@@ -1,12 +1,51 @@
-import { Component, input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { Contact } from '../../interfaces/contact';
+import { ContactService } from '../../services/contacts-service';
+import { Router, RouterModule } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contact-list-item',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './contact-list-item.html',
-  styleUrl: './contact-list-item.scss'
+  styleUrls: ['./contact-list-item.scss']
 })
 export class ContactListItem {
-  contact = input.required<Contact>();
+  @Input() contact!: Contact;
+  router = inject(Router);
+  contactsService = inject(ContactService);
+
+  editContact() {
+    this.router.navigate(['/contacts/edit', this.contact.id]);
+  }
+
+  async deleteContact() {
+    const result = await Swal.fire({
+      title: `¿Eliminar a ${this.contact.firstName}?`,
+      text: 'Esta acción no se puede deshacer',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Sí, eliminar',
+      cancelButtonText: 'Cancelar'
+    });
+
+    if (result.isConfirmed) {
+      try {
+        await this.contactsService.deleteContact(this.contact.id);
+        Swal.fire(
+          'Eliminado!',
+          `${this.contact.firstName} ha sido eliminado.`,
+          'success'
+        );
+      } catch (error) {
+        Swal.fire(
+          'Error!',
+          'No se pudo eliminar el contacto.',
+          'error'
+        );
+      }
+    }
+  }
 }
